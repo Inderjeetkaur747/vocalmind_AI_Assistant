ed4f7cd7ffa895856d04323653bb94ebdd9c2121
Inderjeetkaur747 <71627589+Inderjeetkaur747@users.noreply.github.com>
2025-07-13 18:28:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,23 +13,23 @@ def load_lottie_file(filepath):
         return None
 
 def browser_speak(text):
-    try:
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-    except:
-        html(
-            f"""
-            <script>
-                var msg = new SpeechSynthesisUtterance("{text}");
-                window.speechSynthesis.speak(msg);
-            </script>
-            """,
-            height=0,
-        )
+    st.markdown(
+        f"""
+        <script>
+            var msg = new SpeechSynthesisUtterance("{text}");
+            window.speechSynthesis.cancel();
+            window.speechSynthesis.speak(msg);
+        </script>
+        """,
+        unsafe_allow_html=True,
+    )
 
 def open_website(url):
-    try:
-        webbrowser.open(url)
-    except Exception as e:
-        pass
+    st.markdown(
+        f"""
+        <script>
+            window.open("{url}", "_blank");
+        </script>
+        """,
+        unsafe_allow_html=True,
+    )
